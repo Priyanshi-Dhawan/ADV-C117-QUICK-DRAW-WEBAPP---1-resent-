01ee53ad6edbb0be85e13a2a0b099829cbfd1459
Priyanshi-Dhawan <78725801+Priyanshi-Dhawan@users.noreply.github.com>
2023-03-11 20:05:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log drawn_sketch()
+random_no=Math.floor((math.random()array_1.legnth)+1)
+document.getElementbyId("sketch_to_be_drawn").innerHTML = ("sketch to be drawn"+Element of array);
+timer_counter=0;
+drawn_sketch="";
+timer_check="";
+score=0;
+answer_holder="";
